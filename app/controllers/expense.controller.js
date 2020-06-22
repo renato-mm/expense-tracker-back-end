92c29d332ed8e1d990b84fd23dbf23f3225dfa88
Renato Menezes Machado <renatormm@hotmail.com>
2020-06-22 04:12:08 -0300

    Implemented all controller functions previously declared

--- app/controllers/expense.controller.js
+++ app/controllers/expense.controller.js
@@ -36,30 +36,109 @@ exports.create = (req, res) => {
 
 // Retrieve all Expenses from the database.
 exports.findAll = (req, res) => {
-  
+  const description = req.query.description;
+  var condition = description ? { description: { [Op.like]: `%${description}%` } } : null;
+
+  Expense.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving the Expenses."
+      });
+    });
 };
 
 // Find a single Expense with an id
 exports.findOne = (req, res) => {
-  
+  const id = req.params.id;
+
+  Expense.findByPk(id)
+  .then(data => {
+    res.send(data);
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: err || "Error retrieving Expense with id=" + id
+    });
+  });
 };
 
 // Update a Expense by the id in the request
 exports.update = (req, res) => {
-  
+  // Validate request
+  if(!req.body.description) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+  const id = req.params.id;
+
+  // Create a Expense
+  const expense = {
+    description: req.body.description,
+    reference_month: req.body.reference_month,
+    reference_year: req.body.reference_year,
+    payment_date: req.body.payment_date,
+    due_date: req.body.due_date,
+    amount: req.body.amount
+  }
+
+  // Update Expense in the database
+  Expense.update(req.body, { where: { id: id } })
+    .then(num => {
+      if(num == 1){
+        res.send({ message: "Expense was updated successfully." })
+      }
+      else{
+        res.send({ message: `Cannot update Expense with id=${id}. Maybe Expense was not found.` })
+      }
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Error updating Expense with id=" + id
+      });
+    });
+
 };
 
 // Delete a Expense with the specified id in the request
 exports.delete = (req, res) => {
-  
+  const id = req.params.id;
+
+  Expense.destroy({ where: { id: id } })
+  .then(num => {
+    if(num == 1){
+      res.send({ message: "Expense was delete successfully." })
+    }
+    else{
+      res.send({ message: `Cannot delete Expense with id=${id}. Maybe Expense was not found.` })
+    }
+    res.send(data);
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: err || "Could not delete Expense with id=" + id
+    });
+  });
 };
 
 // Delete all Expenses from the database.
 exports.deleteAll = (req, res) => {
-  
-};
-
-// Find all published Expenses
-exports.findAllPublished = (req, res) => {
-  
+  Expense.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Expenses were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all expenses."
+      });
+    });
 };
